4497558ba3298245808d0678c5257dc120225388
Aditya <aditya07.11.04.12@gmail.com>
2025-02-23 21:18:57 +0530

    added monitor-viewing feature

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ menuCloseButton.addEventListener("click" ,() =>  menuOpenButton.click())
 
 navLinks.forEach(link => {
     link.addEventListener("click", () => menuOpenButton.click());
-    
+
 });
 
 
@@ -28,3 +28,47 @@ const swiper = new Swiper('.slider-wrapper', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  let socket;
+  const canvas = document.getElementById("screenCanvas");
+  const ctx = canvas.getContext("2d");
+
+  const defaultBackground = "url('tv-background.png')";
+  canvas.style.backgroundImage = defaultBackground;
+  canvas.style.backgroundSize = "cover"; 
+  canvas.style.backgroundPosition = "center";
+
+  function connectToServer() {
+      const ip = document.getElementById("ipInput").value;
+      if (!ip) {
+          alert("Please enter a valid IP address!");
+          return;
+      }
+
+      socket = new WebSocket(`ws://${ip}:8765`);
+      socket.binaryType = "arraybuffer";
+
+      socket.onmessage = (event) => {
+          const blob = new Blob([event.data], { type: "image/png" });
+          const img = new Image();
+          img.src = URL.createObjectURL(blob);
+
+          img.onload = () => {
+              canvas.width = img.width;
+              canvas.height = img.height;
+              ctx.drawImage(img, 0, 0);
+              URL.revokeObjectURL(img.src);
+          };
+      };
+
+      socket.onclose = () => console.log("Connection closed");
+  }
+
+  function disconnectFromServer() {
+    if (socket) {
+        socket.close();
+        console.log("Disconnected from server");
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+            canvas.style.backgroundImage = defaultBackground;
+}
